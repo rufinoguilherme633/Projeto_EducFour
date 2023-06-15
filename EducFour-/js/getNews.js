03c5867c806463c7f13d0cb9c9b9f359a6482588
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-06-15 09:00:44 -0300

    alguns ajustes

--- EducFour-/js/getNews.js
+++ EducFour-/js/getNews.js
@@ -1,6 +1,16 @@
 "use strict";
 
 async function getNewspaper() {
+  const urlNewspaper = "http://localhost:5050/v1/educ_four/news/get";
+  const response = await fetch(urlNewspaper);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+"use strict";
+
+async function getnewspaper() {
   const urlnewspaper = "http://localhost:5050/v1/educ_four/news/get";
   const response = await fetch(urlnewspaper);
   const data = await response.json();
@@ -8,67 +18,92 @@ async function getNewspaper() {
   return data;
 }
 
+getnewspaper();
+
 const criarCards = (newspaper) => {
   const card = document.createElement("div");
   card.classList.add("new");
+
   const img = document.createElement("img");
   img.classList.add("news_img");
   img.src = newspaper.capa_noticia; // Defina a URL da imagem
+
   const containernewspaper = document.createElement("div");
   containernewspaper.classList.add("news_content");
+
   const title = document.createElement("p");
   title.classList.add("title_news");
   title.textContent = newspaper.titulo; // Defina o título da notícia
+
   const description = document.createElement("p");
   description.classList.add("news_description");
   description.textContent = newspaper.descricao; // Defina a descrição da notícia
+
   const date = document.createElement("span");
   date.classList.add("date");
   date.textContent = newspaper.data_noticia; // Defina a data da notícia
-  const body_news = document.createElement("span");
-  body_news.classList.add("body_news");
-  body_news.textContent = newspaper.corpo_noticia;
+
+  const body_news = document.createElement("span")
+  body_news.classList.add("body_news")
+  body_news.textContent = newspaper.corpo_noticia
+
   containernewspaper.appendChild(title);
   containernewspaper.appendChild(description);
   containernewspaper.appendChild(date);
-  containernewspaper.appendChild(body_news);
+  containernewspaper.appendChild(body_news)
   card.appendChild(img);
   card.appendChild(containernewspaper);
+
+  const newsContainer = document.querySelector(".news_container");
+  newsContainer.appendChild(card);
+
   return card;
 };
 
+
+const load_all_newspapers = async () => {
+  const container = document.getElementById("news");
+  const cards = await getnewspaper();
+  
+  const news = cards.news.map(criarCards);
+  
+  container.append(...news);
+};
+
+
+
+load_all_newspapers()
+
+
 const loadAllNewspapers = async () => {
   const container = document.getElementById("news_container");
-  const cards = await getNewspaper();
-  const news = cards.news.map(criarCards);
-  container.append(...news);
-};
-
-const orderNews = (news, order) => {
-  if (order === "desc") {
-    news.sort((a, b) => {
-      const dateA = new Date(a.data_noticia.split("/").reverse().join("-"));
-      const dateB = new Date(b.data_noticia.split("/").reverse().join("-"));
-      return dateB - dateA;
-    });
-  }
-  return news;
-};
-
-const handleOrderChange = async () => {
   const select = document.getElementById("order-select");
-  const order = select.value;
-  const container = document.getElementById("news_container");
-  container.innerHTML = "";
+  const newspapers = await getNewspaper();
 
-  const cards = await getNewspaper();
-  const orderedNews = orderNews(cards.news, order);
-  const news = orderedNews.map(criarCards);
-  container.append(...news);
+  const ordenarNoticias = () => {
+    const opcaoSelecionada = select.value;
+    if (opcaoSelecionada === "asc") {
+      newspapers.news.sort((a, b) => a.data_noticia.localeCompare(b.data_noticia));
+    } else if (opcaoSelecionada === "desc") {
+      newspapers.news.sort((a, b) => b.data_noticia.localeCompare(a.data_noticia));
+    }
+  };
+
+  select.addEventListener("change", () => {
+    ordenarNoticias();
+    renderNoticias();
+  });
+
+  const renderNoticias = () => {
+    container.innerHTML = "";
+    newspapers.news.forEach((newspaper) => {
+      const card = criarCards(newspaper);
+      container.appendChild(card);
+    });
+  };
+
+  ordenarNoticias();
+  renderNoticias();
 };
-
-// Event listener para a mudança de seleção no <select>
-const orderSelect = document.getElementById("order-select");
-orderSelect.addEventListener("change", handleOrderChange);
 
 loadAllNewspapers();
